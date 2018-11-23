a35bbf2f4ca769a7a42c3c6a61209f4b5f4a355c
Martin <Mous.imp@gmail.com>
2018-11-23 01:12:54 +0100

    change to more faster and ugly written algorithm

--- beeclust/utils.pyx
+++ beeclust/utils.pyx
@@ -1,10 +1,11 @@
 import cython
-import numpy as np
-from libcpp.pair cimport pair
 from libcpp.deque cimport deque
 from libcpp.set cimport set
 
+from cython.view cimport array as carray
+
 cimport numpy as cnp
+from libc.math cimport NAN, INFINITY
 
 cdef int AMNESIA = -1
 cdef int EMPTY = 0
@@ -16,18 +17,13 @@ cdef int WALL = 5
 cdef int HEATER = 6
 cdef int COOLER = 7
 
-cdef list BEE = [
-        BEE_UP,
-        BEE_RIGHT,
-        BEE_DOWN,
-        BEE_LEFT,
-]
+cdef extern from "state.h":
+    cdef cppclass State:
+        State() except +
+        State(int, int, int, int) except +
+        int x, y, d, t
+        int operator<(const State& rhs) const
 
-cdef list OBSTACLES = [
-        WALL,
-        HEATER,
-        COOLER,
-]
 
 def check_type(check, targets, key):
     for target in targets:
@@ -37,98 +33,200 @@ def check_type(check, targets, key):
     raise TypeError(f'{key} is invalid type')
 
 # [[x,y], [shape, step]]
-ctypedef pair[pair[int, int], pair[pair[int, int],int]] state
 
-cdef state new_state(state a):
-    return state(pair[int,int](a.first.first, a.first.second),
-                 pair[pair[int, int],int](a.second.first, a.second.second))
+cdef State new_state(State a):
+    return State(a.x, a.y, a.d + 1, a.t)
 
-cdef state move_up(state a):
-    if a.first.first - 1 >= 0:
+cdef State move_up(State a):
+    if a.x - 1 >= 0:
         new = new_state(a)
-        new.first.first -= 1
+        new.x -= 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_left(state a):
-    if a.first.second - 1 >= 0:
+cdef State move_left(State a):
+    if a.y - 1 >= 0:
         new = new_state(a)
-        new.first.second -= 1
+        new.y -= 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_right(state a):
-    if a.first.second + 1 < a.second.first.second:
+cdef State move_right(State a, int y_max):
+    if a.y + 1 < y_max:
         new = new_state(a)
-        new.first.second += 1
+        new.y += 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_down(state a):
-    if a.first.first + 1 < a.second.first.first:
+cdef State move_down(State a, int x_max):
+    if a.x + 1 < x_max:
         new = new_state(a)
-        new.first.first += 1
+        new.x += 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_up_right(state a):
-    if a.first.first - 1 >= 0 and a.first.second + 1 < a.second.first.second:
+cdef State move_up_right(State a, int y_max):
+    if a.x - 1 >= 0 and a.y + 1 < y_max:
         new = new_state(a)
-        new.first.first -= 1
-        new.first.second += 1
+        new.x -= 1
+        new.y += 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_up_left(state a):
-    if a.first.first - 1 >= 0 and a.first.second - 1 >= 0:
+cdef State move_up_left(State a):
+    if a.x - 1 >= 0 and a.y - 1 >= 0:
         new = new_state(a)
-        new.first.first -= 1
-        new.first.second -= 1
+        new.x -= 1
+        new.y -= 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_down_right(state a):
-    if a.first.first + 1 < a.second.first.first and a.first.second + 1 < a.second.first.second:
+cdef State move_down_right(State a, int x_max, int y_max):
+    if a.x + 1 < x_max and a.y + 1 < y_max:
         new = new_state(a)
-        new.first.first += 1
-        new.first.second += 1
+        new.x += 1
+        new.y += 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
-cdef state move_down_left(state a):
-    if a.first.first + 1 < a.second.first.first and a.first.second - 1 >= 0:
+cdef State move_down_left(State a, int x_max):
+    if a.x + 1 < x_max and a.y - 1 >= 0:
         new = new_state(a)
-        new.first.first += 1
-        new.first.second -= 1
+        new.x += 1
+        new.y -= 1
         return new
     else:
-        return state(pair[int, int](0, 0),
-                     pair[pair[int, int], int](pair[int, int](0,0), -1))
+        return State(0, 0, 0, -1)
 
 @cython.boundscheck(False)
 def calculate_heatmap(double[:, :] heatmap_view,  cnp.int64_t[:, :] map_view,
                       int T_heater, int T_cooler, int T_env, float k_temp):
     cdef int row, col, i
-    cdef int dist_heater, dist_cooler
+    cdef double dist_heater, dist_cooler
     cdef double heating, cooling
-    cdef deque[state] queue
-    cdef state move, init, start
-    cdef set[state] visited
+    cdef deque[State] queue
+    cdef State move, init, start
+    cdef set[State] visited
+    cdef double[:, :] heaters = carray((heatmap_view.shape[0], heatmap_view.shape[1]),sizeof(double), 'd')
+    cdef double[:, :] coolers = carray((heatmap_view.shape[0], heatmap_view.shape[1]),sizeof(double), 'd')
 
+    # finding heaters and coolers
+    for row in range(map_view.shape[0]):
+        for col in range(map_view.shape[1]):
+            if map_view[row, col] == HEATER:
+                heaters[row, col] = 0
+                queue.push_back(State(row, col, 0, HEATER))
+            elif map_view[row, col] == COOLER:
+                coolers[row, col] = 0
+                queue.push_back(State(row, col, 0, COOLER))
+            elif map_view[row, col] == WALL:
+                coolers[row, col] = INFINITY
+                heaters[row, col] = INFINITY
+            else:
+                coolers[row, col] = -1
+                heaters[row, col] = -1
+
+    while queue.size() > 0:
+        start = queue.front()
+        queue.pop_front()
+
+        move = move_up(start)
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_left(start)
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_right(start, map_view.shape[1])
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_down(start, map_view.shape[0])
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_up_right(start, map_view.shape[1])
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_up_left(start)
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_down_right(start, map_view.shape[0], map_view.shape[1])
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+        move = move_down_left(start, map_view.shape[0])
+        if move.t != -1 and map_view[move.x, move.y] != WALL:
+            if move.t == HEATER:
+                if heaters[move.x, move.y] < 0 or heaters[move.x, move.y] > move.d:
+                    heaters[move.x, move.y] = move.d
+                    queue.push_back(move)
+            if move.t == COOLER:
+                if coolers[move.x, move.y] < 0 or coolers[move.x, move.y] > move.d:
+                    coolers[move.x, move.y] = move.d
+                    queue.push_back(move)
+
+
+    # calculating the temperatures for empty cells
     for row in range(map_view.shape[0]):
         for col in range(map_view.shape[1]):
             if map_view[row, col] == HEATER:
@@ -136,78 +234,13 @@ def calculate_heatmap(double[:, :] heatmap_view,  cnp.int64_t[:, :] map_view,
             elif map_view[row, col] == COOLER:
                 heatmap_view[row, col] = T_cooler
             elif map_view[row, col] == WALL:
-                heatmap_view[row, col] = np.nan
+                heatmap_view[row, col] = NAN
             else:
-                dist_cooler = map_view.shape[0] * map_view.shape[1]
-                dist_heater = map_view.shape[0] * map_view.shape[1]
-                # queue.append(Coordinates(row, col, np_map.shape))
+                dist_cooler = coolers[row, col]
+                dist_heater = heaters[row, col]
 
-                init = state(pair[int,int](row, col),
-                             pair[pair[int, int],int](pair[int, int](map_view.shape[0], map_view.shape[1]), 0))
-                queue.clear()
-                queue.push_back(init)
-
-                visited.clear()
-
-                while queue.size() > 0:
-                    start = queue.front()
-                    queue.pop_front()
-
-                    if visited.find(start) == visited.end():
-                        visited.insert(start)
-                    else:
-                        continue
-                    if start.second.second > dist_cooler or start.second.second > dist_heater:
-                        continue
-
-                    if map_view[start.first.first, start.first.second] == HEATER:
-                        if start.second.second < dist_heater:
-                            dist_heater = start.second.second
-                        continue
-                    if map_view[start.first.first, start.first.second] == COOLER:
-                        if start.second.second < dist_cooler:
-                            dist_cooler = start.second.second
-                        continue
-                    if map_view[start.first.first, start.first.second] == WALL:
-                        continue
-                    if start.second.second >= map_view.shape[0] * map_view.shape[1]:
-                        break
-                    start.second.second += 1
-
-                    move = move_up(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_left(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_right(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_down(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_up_right(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_up_left(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_down_right(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                    move = move_down_left(start)
-                    if move.second.second != -1:
-                        queue.push_back(move)
-
-                heating = (1 / dist_heater) * (T_heater - T_env)
-                cooling = (1 / dist_cooler) * (T_env - T_cooler)
+                heating = (1 / dist_heater) * <double>(T_heater - T_env)
+                cooling = (1 / dist_cooler) * <double>(T_env - T_cooler)
                 heatmap_view[row, col] = \
-                    T_env + k_temp * (heating - cooling)
-                    # T_env + k_temp * (max(heating, 0) - max(cooling, 0))
+                    T_env + k_temp * (max(heating, 0) - max(cooling, 0))
+                    # T_env + k_temp * (heating - cooling)
